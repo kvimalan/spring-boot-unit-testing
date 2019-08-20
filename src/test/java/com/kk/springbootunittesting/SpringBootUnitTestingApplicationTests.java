1cb425ddd42f3d1bf89067491d2a7a6fc94d03c7
Kuriakose Vimalanathan <kuria.vimalanathan@gmail.com>
2019-08-20 14:30:15 +0530

    Initial commit

---
+++ src/test/java/com/kk/springbootunittesting/SpringBootUnitTestingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kk.springbootunittesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootUnitTestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
